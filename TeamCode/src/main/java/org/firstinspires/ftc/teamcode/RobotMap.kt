52470d875ce2a8740577200c9f932d952062899c
Hamburboy <alonhamb@gmail.com>
2026-04-29 06:59:06 +0300

    exemple balnk project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotMap.kt
@@ -4,9 +4,14 @@ object RobotMap {
 
     /** here you set the hardware ids for your subsystems
      * each subsystem has her own object with its devices and their ids
-     * an id declaration looks like this
+     * an id decleration looks like this
      *  var DEVICE_NAME = "deviceId"
      */
 
+    object ExampleSubsystem {
+        const val EXAMPLE_MOTOR_ID = "example motor"
+        const val EXAMPLE_SERVO_ID = "example servo"
+        const val EXAMPLE_CR_SERVO_ID = "example cr servo id"
+    }
 
 }
