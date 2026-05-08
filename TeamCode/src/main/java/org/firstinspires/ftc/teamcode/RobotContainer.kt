4facbee63b7d31039c03359442a53242c7906d36
Hamburboy <alonhamb@gmail.com>
2026-05-09 00:40:28 +0300

    finished implementing new HaServo.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.kt
@@ -2,12 +2,12 @@ package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.HardwareMap
-import com.seattlesolvers.solverslib.command.Robot
 import com.seattlesolvers.solverslib.gamepad.GamepadEx
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.alonlib.TelemetryLevel
 import org.firstinspires.ftc.teamcode.alonlib.units.Alliance
 import org.firstinspires.ftc.teamcode.subsystems.drive.DriveSubsystem
+import org.firstinspires.ftc.teamcode.subsystems.intake.IntakeSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterSubsystem
 import org.firstinspires.ftc.teamcode.subsystems.transfer.TransferSubsystem
 
@@ -22,18 +22,15 @@ class RobotContainer(
     gamepad2: Gamepad,
     alliance: Alliance,
     telemetryLevel: TelemetryLevel
-) : Robot() {
-
+) {
     val controllerA = GamepadEx(gamepad1)
     val controllerB = GamepadEx(gamepad2)
 
     // --- Subsystem declaration
-    val shooterSubsystem = ShooterSubsystem(hardwareMap, telemetry, telemetryLevel)
-
     val driveSubsystem = DriveSubsystem(hardwareMap, telemetry, telemetryLevel)
-
+    val intakeSubsystem = IntakeSubsystem(hardwareMap, telemetry, telemetryLevel)
     val transferSubsystem = TransferSubsystem(hardwareMap, telemetry, telemetryLevel)
-
+    val shooterSubsystem = ShooterSubsystem(hardwareMap, telemetry, telemetryLevel)
 
     // --- init functions ---
     init {
