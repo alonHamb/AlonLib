7d4d8415babda1a5688be3910aaf00b96116919b
Hamburboy <alonhamb@gmail.com>
2026-05-10 03:47:59 +0300

    cleaned testing stuff merged from dev

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.kt
@@ -3,16 +3,10 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.hardware.Gamepad
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.seattlesolvers.solverslib.command.Robot
-import com.seattlesolvers.solverslib.command.button.GamepadButton
 import com.seattlesolvers.solverslib.gamepad.GamepadEx
-import com.seattlesolvers.solverslib.gamepad.GamepadKeys
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.Commands.testCommand
-import org.firstinspires.ftc.teamcode.Commands.testDefaultCommand
 import org.firstinspires.ftc.teamcode.alonlib.TelemetryLevel
 import org.firstinspires.ftc.teamcode.alonlib.units.Alliance
-import org.firstinspires.ftc.teamcode.alonlib.units.rotations
-import org.firstinspires.ftc.teamcode.subsystems.TestSubsystem
 
 
 class RobotContainer(
@@ -29,8 +23,6 @@ class RobotContainer(
 
     // --- Subsystem declaration
 
-    val testSubsystem = TestSubsystem(hardwareMap, telemetry, telemetryLevel)
-
 
     // --- init functions ---
     init {
@@ -40,7 +32,6 @@ class RobotContainer(
 
     fun configureButtonBindings() {
         with(controllerA) {
-            GamepadButton(this, GamepadKeys.Button.A).whileHeld(testSubsystem.testCommand { controllerA.leftY.rotations })
 
         }
         with(controllerB) {
@@ -51,6 +42,5 @@ class RobotContainer(
     }
 
     fun setDefaultCommands() {
-        testSubsystem.defaultCommand = testSubsystem.testDefaultCommand()
     }
 }
