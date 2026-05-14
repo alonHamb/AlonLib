2103e89e57c335f1a01bc9fc74c6d20cb25f8455
Hamburboy <alonhamb@gmail.com>
2026-05-14 04:13:54 +0300

    working on getting dashboard working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestOpMode.kt
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop
+
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.Config
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry
+import com.qualcomm.hardware.lynx.LynxModule
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.seattlesolvers.solverslib.command.CommandOpMode
+import com.seattlesolvers.solverslib.hardware.motors.Motor
+import org.firstinspires.ftc.teamcode.alonlib.hardware.motors.HaMotor
+import org.firstinspires.ftc.teamcode.alonlib.math.PIDFGains
+import org.firstinspires.ftc.teamcode.opmodes.teleop.TestOpMode.Constants.kD
+import org.firstinspires.ftc.teamcode.opmodes.teleop.TestOpMode.Constants.kF
+import org.firstinspires.ftc.teamcode.opmodes.teleop.TestOpMode.Constants.kI
+import org.firstinspires.ftc.teamcode.opmodes.teleop.TestOpMode.Constants.kP
+import org.firstinspires.ftc.teamcode.opmodes.teleop.TestOpMode.Constants.setPoint
+
+@TeleOp(name = "Test", group = "Teleop")
+@Config
+class TestOpMode : CommandOpMode() {
+    lateinit var motor: HaMotor
+    lateinit var hub: LynxModule
+    override fun initialize() {
+        motor = HaMotor(hardwareMap, "m3", Motor.GoBILDA.RPM_312).apply {
+            runMode = Motor.RunMode.PositionControl
+            zeroPowerBehavior = Motor.ZeroPowerBehavior.FLOAT
+        }
+        hub = hardwareMap.get(LynxModule::class.java, "Control Hub").apply {
+            bulkCachingMode = LynxModule.BulkCachingMode.MANUAL
+        }
+        telemetry = MultipleTelemetry(telemetry, FtcDashboard.getInstance().telemetry)
+        telemetry.addLine("Robot initializing")
+        telemetry.update()
+
+
+    }
+
+    object Constants {
+        @JvmField
+
+        var kP: Double = 0.0
+
+        var kI: Double = 0.0
+
+        var kD: Double = 0.0
+
+        var kF: Double = 0.0
+
+        var setPoint: Double = 0.0
+    }
+
+    override fun run() {
+        hub.clearBulkCache()
+        motor.pidfGains = PIDFGains(kP, kI, kD, kF)
+        motor.setPoint = setPoint
+        super.run()
+        telemetry.update()
+    }
+}
